39ec8509fade7913246199ddb2e6aafeb5697c81
dagrons <heyuehuii@126.com>
2021-11-16 09:37:00 +0800

    add htmlize, make the use-package syntax  more consistent

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,7 +18,7 @@ a/gnu/")
  '(custom-safe-themes
    '("3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" "e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
  '(package-selected-packages
-   '(atomic-chrome iedit org-bullets zenburn-theme use-package dracula-theme)))
+   '(htmlize atomic-chrome iedit org-bullets zenburn-theme use-package dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
