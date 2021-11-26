8890a17016ca7a84db892be73dd40024b1b3d288
dagrons <heyuehuii@126.com>
2021-11-26 20:02:20 +0800

    add more themes, mode-lines, and fixed python indent issues

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -17,6 +17,8 @@
 ;; 执行org-babel
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
+;; 设置默认窗口大小
+(when window-system (set-frame-size (selected-frame) 150 50))
 
 ;; 下面是自动生成的内容
 (custom-set-variables
@@ -25,9 +27,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" "e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" default))
+   '("d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "6bdcff29f32f85a2d99f48377d6bfa362768e86189656f63adbf715ac5c1340b" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163" "76b4632612953d1a8976d983c4fdf5c3af92d216e2f87ce2b0726a1f37606158" default))
+ '(org-agenda-files nil)
  '(package-selected-packages
-   '(ox-reveal graphviz-dot-mode company-graphviz-dot graphviz-dot-indent-mode ob-dot ob-http ob-graphql ob-shell ob-C ob-go lsp-ui gnu-elpa gnu-elpa-keyring-update lsp-mode htmlize atomic-chrome iedit org-bullets zenburn-theme use-package dracula-theme)))
+   '(gruvbox-theme ample-theme twilight-bright-theme doom-modeline smart-mode-line powerline ox-reveal graphviz-dot-mode company-graphviz-dot graphviz-dot-indent-mode ob-dot ob-http ob-graphql ob-shell ob-C ob-go lsp-ui gnu-elpa gnu-elpa-keyring-update lsp-mode htmlize atomic-chrome iedit org-bullets zenburn-theme use-package dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,5 +41,4 @@
 
 (put 'erase-buffer 'disabled nil)
 
-;; 设置默认窗口大小
-(when window-system (set-frame-size (selected-frame) 150 50))
+
