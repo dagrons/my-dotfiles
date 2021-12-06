c702c32f9ad8a7c0047a4f938405d3efdbef1ced
dagrons <heyuehuii@126.com>
2021-12-06 10:55:09 +0800

    add x11

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,4 @@
+(setq x-alt-keysym 'meta)
 ;; 换到清华源, 但有时会产生签名问题, 这时候就暂时换回官方源
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
 			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
@@ -30,7 +31,7 @@
    '("d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "6bdcff29f32f85a2d99f48377d6bfa362768e86189656f63adbf715ac5c1340b" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163" "76b4632612953d1a8976d983c4fdf5c3af92d216e2f87ce2b0726a1f37606158" default))
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(exec-path-from-shell floobits gruvbox-theme ample-theme twilight-bright-theme doom-modeline smart-mode-line powerline ox-reveal graphviz-dot-mode company-graphviz-dot graphviz-dot-indent-mode ob-dot ob-http ob-graphql ob-shell ob-C ob-go lsp-ui gnu-elpa gnu-elpa-keyring-update lsp-mode htmlize atomic-chrome iedit org-bullets zenburn-theme use-package dracula-theme)))
+   '(yaml-mode exec-path-from-shell floobits gruvbox-theme ample-theme twilight-bright-theme doom-modeline smart-mode-line powerline ox-reveal graphviz-dot-mode company-graphviz-dot graphviz-dot-indent-mode ob-dot ob-http ob-graphql ob-shell ob-C ob-go lsp-ui gnu-elpa gnu-elpa-keyring-update lsp-mode htmlize atomic-chrome iedit org-bullets zenburn-theme use-package dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- profile/.profile
+++ profile/.profile
@@ -25,3 +25,6 @@ fi
 if [ -d "$HOME/.local/bin" ] ; then
     PATH="$HOME/.local/bin:$PATH"
 fi
+
+export LANG="zh_CN.UTF-8"
+
